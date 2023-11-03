608390fe1a3784cb8c8398d7ce24ea12bf6a8e7d
Hub Fatec <147555396+HubFatecItape@users.noreply.github.com>
2023-11-03 14:33:10 -0300

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -15,43 +15,6 @@ function updatePosition() {
 }
 requestAnimationFrame(updatePosition);
 
-// document.addEventListener( 'DOMContentLoaded', function () {
-//   var splide = new Splide( '#thumbnail-slider', {
-//     fixedWidth : 480,
-//     fixedHeight: 530,
-//     type: 'loop',
-//     gap        : 5,
-//     rewind     : true,
-//     pagination : false,
-//     cover      : true,
-//     focus      : 'center',
-//     isNavigation: true,
-//     autoplay: 'playing',
-//     pauseOnHover: false,
-//     lazyload: 'loaded'
-//   } 
-//   ).mount();
-
-//   splide.on( 'autoplay:playing', function ( rate ) {
-//     console.log( rate );
-//   } );
-
-//   document.querySelector('.splide__arrow--next').innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/></svg>';
-
-//   document.querySelector('.splide__arrow--prev').innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/></svg>';
-
-//    var urls = ['https://fatecitapetininga.edu.br/', 'https://fatecitapetininga.edu.br/', 'https://fatecitapetininga.edu.br/', 'https://fatecitapetininga.edu.br/', 'https://fatecitapetininga.edu.br/'];
-
-//    var slides = document.querySelectorAll('.splide__slide');
-//    slides.forEach(function(slide, index) {
-//      slide.addEventListener('click', function() {
-//        if (slide.classList.contains('is-active')) {
-//          window.location.href = urls[index]; // Use o índice para acessar o URL correspondente
-//        }
-//      });
-//    });
-// })
-
 document.addEventListener( 'DOMContentLoaded', function () {
   var splide = new Splide( '#thumbnail-slider', {
     fixedWidth : 480,
