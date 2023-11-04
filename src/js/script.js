08decd5a44ca252a4cffb6f894889f6abd177f39
Hub Fatec <147555396+HubFatecItape@users.noreply.github.com>
2023-11-04 17:15:47 -0300

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -28,7 +28,14 @@ document.addEventListener( 'DOMContentLoaded', function () {
     isNavigation: true,
     autoplay: 'playing',
     pauseOnHover: false,
-    lazyload: 'loaded'
+    lazyload: 'loaded',
+    breakpoints: {
+      800: {
+          // configurações para telas menores que 600px
+          perPage: 3,
+          arrows: false,
+      }
+  }
   } 
   ).mount();
 
@@ -40,5 +47,3 @@ document.addEventListener( 'DOMContentLoaded', function () {
 
   document.querySelector('.splide__arrow--prev').innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/></svg>';
 })
-
-
