effb371692b36e19d147d4bd52c8d2295076b7e6
gabrielliima2 <gabrielantuneslima10@gmail.com>
2024-08-02 12:45:15 -0300

    modified:   ads.html 	modified:   agronegocio.html 	modified:   comex.html 	modified:   egressos.html 	modified:   ga.html 	modified:   gpi.html 	modified:   index.html 	modified:   projetos/ads/siteex.html 	new file:   src/assets/menu.svg 	modified:   src/css/style.css 	modified:   src/js/script.js

--- src/js/script.js
+++ src/js/script.js
@@ -98,9 +98,12 @@ window.onload = function() {
   }, 5000);
 };
 
-
-
-
+const itensMenu = document.querySelector('.itensMenu')
+const menu = document.querySelector('.menu')
+menu.addEventListener('click',()=>{
+  itensMenu.classList.toggle('hide')
+  menu.classList.toggle('bgMenu')
+})
 
 
 
